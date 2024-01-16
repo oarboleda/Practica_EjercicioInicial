544a2285486f8bfb1a87ed14c26b4596c464227f
oarboleda <orarbole@hotmail.com>
2024-01-16 16:24:53 -0500

    Prueba de modificacion del archivo

--- src/Modelo/EstudianteTecnologia.java
+++ src/Modelo/EstudianteTecnologia.java
@@ -3,7 +3,7 @@ package Modelo;
 /*
 *  Clase EstudianteTecnologia
 *  Creado por: Orlando Arboleda Molina
-*  Actualizado: 15-Enero-2025
+*  Actualizado: 16-Enero-2025
 *
 *  Descripción: 
 *  Clase que es la implementación de la clase para la construcción de los 
